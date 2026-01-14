f325a62478c7f553062179840fb8db5b1177323d
Fabricio <fabriciodev28@gmail.com>
2026-01-14 20:22:55 -0300

    Adicionando container e estilo de layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "./components/pages/Home"
+import Projects from "./components/pages/Projects"
 import Company from "./components/pages/Company"
 import Contact from "./components/pages/Contact"
 import NewProject from "./components/pages/NewProject"
 
 
+import Container from "./components/layout/Container"
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+
 function App() {
   return (
     <Router>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/company">Company</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/newproject">New Project</Link></li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
-      <p>Footer</p>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   )
 }
