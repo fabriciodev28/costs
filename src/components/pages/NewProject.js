b8ce4a2f7f524eebdae6f06850464925557018b1
Fabricio <fabriciodev28@gmail.com>
2026-01-19 22:41:18 -0300

    Adicionando componentizacao de formulário

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,13 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
+
 function NewProject() {
   return (
-    <div>
-      <h1>Welcome to the New Project Page</h1>
-      <p>This is the new project creation page.</p>
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm btnText="Criar Projeto" />
     </div>
   )
 }
